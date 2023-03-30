7ddfe475ec72a45b9195f5a613db88eb02f63912
carbal23 <mcarbal23@gmail.com>
2023-03-30 18:33:19 -0500

    PROYECTO1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { TaskList } from "./componentes/TaskList";
+import { TaskForm } from "./componentes/TaskForm";
+ 
+function App() {
+  return (
+    <div>
+      <TaskForm/>
+      <TaskList/>
+    </div>
+  );
+}
+
+export default App;
